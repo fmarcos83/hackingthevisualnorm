74d4ad2d39841a616f5053b42d9453b7e5d6c432
Francisco Marcos <fmarcos83@gmail.com>
2017-04-11 01:01:17 +0200

    last update

--- slides/slide_02/orgTreeNetwork.js
+++ slides/slide_02/orgTreeNetwork.js
@@ -1,10 +1,16 @@
 pt.orgTreeNetwork = pt.orgTreeNetwork || {};
+graphData = graphData || {};
 
 pt.orgTreeNetwork.init = function(graph) {
-    pt.orgTreeNetwork.data = graph;
+    pt.orgTreeNetwork.data = graph
+    createGraphData(pt.orgTreeNetwork.data);
 }//init
 
-pt.orgTreeNetwork.treeNetwork= function() {
-    window.createGraphData(pt.orgTreeNetwork.data);
+pt.orgTreeNetwork.treeNetwork = function() {
+    createGraphData(pt.orgTreeNetwork.data);
 }//treeNetwork
+
+pt.orgTreeNetwork.normalNetwork = function() {
+
+}//normalNetwork
 
